03e06efe40e47d2ec3dc332461a7d7d4684e88c5
Ella <bands1186@yahoo.com>
2015-06-11 14:51:25 -0700

    The completed R code for the R Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,72 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## These pair of functions can cache the inverse of a matrix
+## makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
+## cacheSolver computes the inverse of the special "matrix" returned by makeCacheMatrix.  
+## If the inverse has already been calcuated (and the matrix has not changed), the the cachesolve 
+## will retrieve the inverse from the cache.
 
+
+makeCacheMatrix <- function(x = matrix()) {
+        # store the cached value to m
+        # initalize m to NULL
+        m <- NULL
+        
+        # create the matrix 
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        
+        # get the matrix value
+        get <- function() x
+        
+        # store the inversed matrix m in cache
+        setinverse <- function(inverse) m <<-inverse
+        
+        # get the inversed matrix from cache
+        getinverse <- function() m
+        
+        # return the created function to the calling function
+        list(set = set, get=get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## get the inversed matrix from cache memory
+        m <- x$getinverse()
+        
+        # return message to indicate the inversed matrix is in cache
+        if(!is.null(m)) {
+                message("getting cached inverted matrix")
+                
+                # display matrix in consol
+                return(m)
+        }
+ 
+        #otherwise, retrieve the original matrix and store it in matrix
+        matrix <- x$get()
+        
+        
+        # If the matrix is not in square dimension, return error message.
+        tryCatch({
+                # Inverted the square matrix
+                m <- solve(matrix, ...)
+        }, error = function(e)  {
+                # the matrix is not in square 
+                message("Error:  Can not invert the matrix.  It is not in a square invertible matrix")
+        }, finally = {
+                # store the inverted matrix 
+                x$setinverse(m)  
+        })
+ 
+        
+        # Return the inverted matrix
+        m
+        
 }
